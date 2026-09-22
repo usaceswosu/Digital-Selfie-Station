b18e25afece04ed17b8a70667b405124d07be1a2
Caleb Marsh <spinelessfrog@gmail.com>
2026-09-21 19:15:38 -0500

    fixed placement of some calls

--- js/index.js
+++ js/index.js
@@ -2,27 +2,33 @@ let currentFacingMode = 'environment';
 let activeStream = null;
 
 async function startCamera(facingMode) {
-      if (activeStream) {
-        activeStream.getTracks().forEach(track => track.stop());
-      }
+    if (activeStream) {
+      activeStream.getTracks().forEach(track => track.stop());
+    }
 
-      const constraints = {
+    const constraints = {
         video: {
-          facingMode: { ideal: facingMode },
-          width: { ideal: 1920 },
-          height: { ideal: 1080 }
+            facingMode: { ideal: facingMode },
+            width: { ideal: 1920 },
+            height: { ideal: 1080 }
         },
         audio: false
-      };
+    };
 
-      try {
+    try {
         activeStream = await navigator.mediaDevices.getUserMedia(constraints);
         video.srcObject = activeStream;
-      } catch (err) {
+    } catch (err) {
         console.error("Camera access error:", err);
         alert("Unable to access camera. Please check browser permissions.");
-      }
-
-      
-      startCamera(currentFacingMode);
     }
+}
+
+snapBtn.addEventListener('click', captureCompositeFrame);
+timerBtn.addEventListener('click', () => runTimerSequence(5));
+flipBtn.addEventListener('click', () => {
+    currentFacingMode = currentFacingMode === 'user' ? 'environment' : 'user';
+    startCamera(currentFacingMode);
+});
+
+startCamera(currentFacingMode);
